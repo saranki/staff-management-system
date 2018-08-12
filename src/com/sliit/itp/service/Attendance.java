e3efc8dd9baf3e29ede60edabd491b41b4ef64ee
Saranki M <m.saranki@gmail.com>
2018-08-12 13:41:26 +0530

    Implemented Attendance UI

---
+++ src/com/sliit/itp/service/Attendance.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sliit.itp.service;
+
+/**
+ *
+ * @author user
+ */
+public class Attendance {
+    public long getCurrentDateTime(){     
+        return System.currentTimeMillis();
+    }
+}
